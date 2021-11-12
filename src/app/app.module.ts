9d633a7dccd61452773b6e6e1caf7d362198b574
reikale <sviackeviciute@gmail.com>
2021-11-12 21:37:07 +0200

    Navigation bar for mobile view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
